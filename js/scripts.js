be06855f4fb72d83da135344e29f342e831e793c
JM <143664295+sharkleadership@users.noreply.github.com>
2026-03-11 23:23:38 -0500

    Add JS for Japanese greeting

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// JM, 03/11/26
+"use strict";
+
+const currentHour = function() {
+    const hour = new Date().getHours();
+    return hour;
+}
+setInterval(currentHour, 1000);
+currentHour();
+
+function japaneseGreeting() {
+    const GREETINGS = document.querySelectorAll(".morning, .afternoon, .evening");
+    for (let greet of GREETINGS) {
+        greet.classList.remove("time");
+    }
+    if (currentHour >= 0 && currentHour < 10) {
+        GREETINGS[0].classList.add("time");
+    } else if (currentHour >= 11 && currentHour <= 17) {
+        GREETINGS[1].classList.add("time");
+    } else {
+        GREETINGS[2].classList.add("time");
+    }
+
+    setInterval(japaneseGreeting, 1000);
+}
+japaneseGreeting();
